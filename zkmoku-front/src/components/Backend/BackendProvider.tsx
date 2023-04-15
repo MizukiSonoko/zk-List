c514c34d07b6c944976c129e09b80616d889f5c4
Sonoko Mizuki <sonoko@mizuki.io>
2023-04-15 21:29:14 +0900

    [front] update backend url

--- zkmoku-front/src/components/Backend/BackendProvider.tsx
+++ zkmoku-front/src/components/Backend/BackendProvider.tsx
@@ -4,7 +4,7 @@ import { ServiceType } from "@bufbuild/protobuf";
 import { createPromiseClient, createConnectTransport, PromiseClient } from "@bufbuild/connect-web";
 
 const transport = createConnectTransport({
-  baseUrl: "http://localhost:8080", // "https://zkmokubackend.onrender.com"
+  baseUrl: "https://zkmokubackend.onrender.com"
 });
 
 export function useClient<T extends ServiceType>(service: T): PromiseClient<T> {
